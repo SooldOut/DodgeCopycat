9c39f88b46201b04ad345d0ccd01d61b407b8f69
SooldOut <abracadabra9834@gmail.com>
2025-09-10 10:23:26 +0900

    202509101022_Init
    깃허브 연동 테스트

---
+++ Assets/01_Scripts/GithubTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 고구마맛탕 연근조림 더덕구이 오징어젓갈 검은콩자반
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
